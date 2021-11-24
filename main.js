d7f9c0ecd2887396b134480a6f7e07fc43ba7bab
Hazem Kawas <hazem.k.4488@gmail.com>
2021-11-24 10:02:35 +0100

    add function for changing the nav bar background color

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let activeImageIndex = 0;
 function main() {
     toggleMenu();
     startSlider();
+    changeNavBackgroundColor()
 }
 
 /** show and hide nav item */
@@ -34,4 +35,25 @@ function fadeToNextSlide() {
     const nextImage = images[activeImageIndex];
     activeImage.classList.toggle('show');
     nextImage.classList.toggle('show');
+}
+
+// changing the background color of the nav
+
+const header = document.querySelector('header');
+const sectionOne = document.querySelector('.slider');
+
+function changeNavBackgroundColor() {
+    
+const sectionOneObserver = new IntersectionObserver(function(entries, sectionOneObserver){
+    entries.forEach(entry => {
+       if (!entry.isIntersecting) {
+           header.classList.add('nav-scrolled');
+       } else {
+           header.classList.remove('nav-scrolled');
+       }
+    })
+});
+
+sectionOneObserver.observe(sectionOne)
+
 }
